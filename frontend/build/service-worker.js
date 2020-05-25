546de30fe3fc53adce8f5366179d4855bbfac821
mateodevia <m.devia@uniandes.edu.co>
2020-05-25 11:37:10 -0500

    New build

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.704131bf40f6fc6c49d9ebda990690a2.js"
+  "/precache-manifest.cb727dfc920c5f54b57ddfd7c716f7c5.js"
 );
 
 self.addEventListener('message', (event) => {
